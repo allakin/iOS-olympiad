a2ca60e29eb9ac2ffb82de10b4e7cef00d5b5bf5
shigapovaa <shigapova.alsu@gmail.com>
2017-11-11 10:24:55 +0300

    foundation project

---
+++ SwiftOlymp/Views/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  SwiftOlymp
+//
+//  Created by itisioslab on 11.11.17.
+//  Copyright © 2017 iOS Lab ITIS. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
