5fe762ac0812684fffe0c2f584be5b1316dbc2a1
Тимур Шафигуллин <timursafigullin@Timurs-Mac.local>
2017-11-12 16:09:37 +0300

    need fix cell contraints

--- SwiftOlymp/Controllers/NewsfeedTableViewController.swift
+++ SwiftOlymp/Controllers/NewsfeedTableViewController.swift
@@ -11,9 +11,33 @@ import UIKit
 class NewsfeedTableViewController: UITableViewController {
     
     var news = [News]()
+    let postCellIdentifier = "postCell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        prepareTableView()
+        generateData()
+    }
+    
+    //MARK: - Prepare methods
+    
+    func prepareTableView() {
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 100
+        
+        let postNib = UINib(nibName: "PostTableViewCell", bundle: nil)
+        tableView.register(postNib, forCellReuseIdentifier: postCellIdentifier)
+    }
+    
+    func generateData() {
+        let source = Source(name: "iOS Developer", avatarImageURL: URL(string: "vk.com")!)
+        let vkNews1 = VkNews(source: source, date: Date(), text: "Post with text", imagesURL: nil, videosURL: nil, likeCount: 5, commentCount: 9, repostCount: 11, audiosURL: nil, links: nil)
+        let vkNews2 = VkNews(source: source, date: Date(), text: nil, imagesURL: [(URL(string: "vk.com")!)], videosURL: nil, likeCount: 78, commentCount: 6, repostCount: 43, audiosURL: nil, links: nil)
+        let vkNews3 = VkNews(source: source, date: Date(), text: "Post with text and Image", imagesURL: [(URL(string: "vk.com")!)], videosURL: nil, likeCount: 65, commentCount: 5, repostCount: 98, audiosURL: nil, links: nil)
+        
+        news.append(vkNews1)
+        news.append(vkNews2)
+        news.append(vkNews3)
     }
 
     // MARK: - Table view data source
@@ -23,6 +47,14 @@ class NewsfeedTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return news.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: postCellIdentifier, for: indexPath) as! PostTableViewCell
+        
+        cell.prepareCell(with: news[indexPath.row])
+        
+        return cell
     }
 }
--- SwiftOlymp/Views/PostTableViewCell.swift
+++ SwiftOlymp/Views/PostTableViewCell.swift
@@ -9,16 +9,65 @@
 import UIKit
 
 class PostTableViewCell: UITableViewCell {
-
+    
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var textContentLabel: UILabel!
+    @IBOutlet weak var photoCollectionView: UICollectionView!
+    @IBOutlet weak var likeCountLabel: UILabel!
+    @IBOutlet weak var commentCountLabel: UILabel!
+    @IBOutlet weak var repostCountLabel: UILabel!
+    
+    @IBOutlet weak var avatarToCollectionConstraint: NSLayoutConstraint!
+    @IBOutlet weak var textToLikeConstraint: NSLayoutConstraint!
+    @IBOutlet weak var avatarToTextConstraint: NSLayoutConstraint!
+    @IBOutlet weak var textToCollectionConstraint: NSLayoutConstraint!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func prepareCell(with news: News) {
+        prepareConstraints(with: news)
+        
+        let source = news.source
+        //avatar
+        nameLabel.text = source.name
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        dateLabel.text = dateFormatter.string(from: news.date)
+        
+        if let text = news.text {
+            textContentLabel.text = text
+        }
+        //collectionView
+        likeCountLabel.text = String(news.likeCount)
+        commentCountLabel.text = String(news.commentCount)
+        repostCountLabel.text = String(news.repostCount)
+    }
+    
+    private func prepareConstraints(with news: News) {
+        if news.text == nil {
+            textContentLabel.isHidden = true
+            avatarToTextConstraint.priority = .defaultLow
+            avatarToCollectionConstraint.priority = .defaultHigh
+        } else {
+            textContentLabel.isHidden = false
+            avatarToTextConstraint.priority = .defaultHigh
+            avatarToCollectionConstraint.priority = .defaultLow
+        }
+        
+        if news.text != nil, news.imagesURL == nil {
+            photoCollectionView.isHidden = true
+            textToCollectionConstraint.priority = .defaultLow
+            textToLikeConstraint.priority = .defaultHigh
+        } else {
+            photoCollectionView.isHidden = false
+            textToCollectionConstraint.priority = .defaultHigh
+            textToLikeConstraint.priority = .defaultLow
+        }
     }
     
 }
